b6eef0b8a0773dcb69275100658168ac3945bf20
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-03-26 22:39:24 +0530

    update UDP code

--- User Datagram Protocol/UDPclient.c
+++ User Datagram Protocol/UDPclient.c
@@ -10,34 +10,40 @@ Compile Using: gcc UDPclient.c -o client
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(){
-	char buffer[100];
-	int k; // Error Flag
-	int sock_desc;
-	struct sockaddr_in server, client;
-	
-	/*Creating Socket*/
-	sock_desc = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sock_desc == -1){
-		printf("Error In Socket Creation!");
-		return 1;
-	}
-	printf("Socket Created Successfully!\n");
-	
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(3004);
-	
-	/*Data Sending Process*/
-	printf("\nEnter The Data To Be Sent: ");
-	fgets(buffer, 100, stdin);
-		
-	k = sendto(sock_desc, buffer, strlen(buffer), 0, (struct sockaddr *)&server, sizeof(server));
-	if (k == -1)
-		printf("Error In Sending Data!");
-	printf("Message Sent Successfully!\n");
-		
-	close(sock_desc);
-	
-	return 0;
+int main() {
+    char buffer[100];
+    int status; // Error flag
+    int client_socket;
+    struct sockaddr_in server_addr;
+
+    /* Creating Socket */
+    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_socket == -1) {
+        printf("Error in socket creation!\n");
+        return 1;
+    }
+    printf("Socket created successfully!\n");
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(3004); // Ensure correct byte order
+
+    /* Data Sending Process */
+    printf("\nEnter the data to be sent: ");
+    fgets(buffer, sizeof(buffer), stdin);
+
+    status = sendto(client_socket, buffer, strlen(buffer), 0, 
+                    (struct sockaddr *)&server_addr, sizeof(server_addr));
+    
+    if (status == -1) {
+        printf("Error in sending data!\n");
+        return 1;
+    }
+    
+    printf("Message sent successfully!\n");
+
+    /* Closing the Socket */
+    close(client_socket);
+
+    return 0;
 }
--- User Datagram Protocol/UDPserver.c
+++ User Datagram Protocol/UDPserver.c
@@ -1,8 +1,3 @@
-/*
-Compile Using: gcc UDPserver.c -o server
-./server 3004
-*/
-
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -10,52 +5,51 @@ Compile Using: gcc UDPserver.c -o server
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    int sock_desc;
+#define PORT 3004  // Hardcoded port number
+
+int main() {
+    int server_socket;
     char buffer[100];
     socklen_t client_len;
-    int k; // For error checking
-    struct sockaddr_in server, client;
-    
-    if (argc != 2) {
-        printf("Usage: %s <port>\n", argv[0]);
-        return 1;
-    }
+    int status;
+    struct sockaddr_in server_addr, client_addr;
 
     /* Creating Socket */
-    sock_desc = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock_desc == -1) {
-        perror("Error In Socket Creation");
+    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_socket == -1) {
+        perror("Error in socket creation");
         return 1;
     }
-    printf("Socket Created Successfully!\n");
+    printf("Socket created successfully!\n");
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(atoi(argv[1])); // Get Port from Argument
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);  // Using hardcoded port
 
     /* Binding Process */
-    k = bind(sock_desc, (struct sockaddr *)&server, sizeof(server));
-    if (k == -1) {
-        perror("Error In Binding");
-        close(sock_desc);
+    status = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (status == -1) {
+        perror("Error in binding");
+        close(server_socket);
         return 1;
     }
-    printf("Socket Binded Successfully!\n");
+    printf("Socket bound to port %d successfully!\n", PORT);
 
-    client_len = sizeof(client);
+    client_len = sizeof(client_addr);
 
     /* Receiving Process */
-    k = recvfrom(sock_desc, buffer, 100, 0, (struct sockaddr *)&client, &client_len);
-    if (k < 0) {
-        perror("Error In Receiving Data");
-        close(sock_desc);
+    status = recvfrom(server_socket, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&client_addr, &client_len);
+    if (status < 0) {
+        perror("Error in receiving data");
+        close(server_socket);
         return 1;
     }
-    printf("Listening....\n");
 
-    printf("Datagram From Client: %s\n", buffer);
-    
-    close(sock_desc);
+    printf("Received Message: %s\n", buffer);
+
+    /* Closing the Socket */
+    close(server_socket);
+
     return 0;
 }
