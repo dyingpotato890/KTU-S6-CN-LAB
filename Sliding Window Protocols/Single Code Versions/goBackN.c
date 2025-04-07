51a34cfa1db50375401fd53268cc0bbff5d33807
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-04-07 12:41:28 +0530

    Rework Logic For Go Back N

--- Sliding Window Protocols/Single Code Versions/goBackN.c
+++ Sliding Window Protocols/Single Code Versions/goBackN.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
+#include <time.h>
+
+#define MAX_PACKETS 100
 
 int main() {
-  srand(time(NULL));
-  
-  int windowSize, n, base = 0, timer;
+    srand(time(NULL));
 
-  printf("Enter Total Number Of Frames: ");
-  scanf("%d", &n);
-  printf("Enter Timer (In Sec): ");
-  scanf("%d", &timer);
-  printf("Enter Window Size: ");
-  scanf("%d", &windowSize);
+    int total_packets, timer, window_size;
+    printf("Enter Total Number Of Frames: ");
+    scanf("%d", &total_packets);
+    printf("Enter Timer (In Sec): ");
+    scanf("%d", &timer);
+    printf("Enter Window Size: ");
+    scanf("%d", &window_size);
 
-  while (base < n) {
-    printf("Sending packet %d to receiver\n", base);
-    sleep(timer);
+    int base = 0; // First unacknowledged packet
+    int next_seq = 0; // Next packet to send
 
-    if (rand() % 10 < 2) {
-      printf("Acknowledgement of packet %d has not been received\n", base);
-      sleep(1);
+    while (base < total_packets) {
+        while (next_seq < base + window_size && next_seq < total_packets) {
+            printf("Sending Packet %d To Receiver\n", next_seq);
+            sleep(timer);
+            next_seq++;
+        }
 
-      for (int i = base + 1; i < base + windowSize && i < n; i++) {
-        printf("Sending packet %d to receiver\n", i);
-        printf("Packet %d has been dropped by receiver\n", i);
-        sleep(1);
-      }
-    } else {
-      printf("Acknowledgement for packed %d has been received\n", base);
-      base++;
+        int lost = (rand() % 10) < 3;
+        if (lost) {
+            printf("ACK for Packet %d Not Received! Retransmitting...\n", base);
+            next_seq = base; // Reset next_seq to retransmit from base
+        } else {
+            printf("ACK for Packet %d Received. Window Advanced.\n", base);
+            base++;
+        }
     }
-  }
 
-  return 0;
+    printf("All Packets Transmitted Successfully!\n");
+    return 0;
 }
