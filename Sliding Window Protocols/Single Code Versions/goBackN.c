6df9f8619ddf4f306930704348cb66645c8735a6
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-04-07 12:29:19 +0530

    Rework Logic For Go Back N

--- Sliding Window Protocols/Single Code Versions/goBackN.c
+++ Sliding Window Protocols/Single Code Versions/goBackN.c
@@ -1,77 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
 
-struct node {
-    int packet;
-    struct node* next;
-};
+int main() {
+  srand(time(NULL));
+  
+  int windowSize, n, base = 0, timer;
 
-struct node* head = NULL;
+  printf("Enter Total Number Of Frames: ");
+  scanf("%d", &n);
+  printf("Enter Timer (In Sec): ");
+  scanf("%d", &timer);
+  printf("Enter Window Size: ");
+  scanf("%d", &windowSize);
 
-void addNode(int packet) {
-    struct node* temp = head;
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    newNode->packet = packet;
+  while (base < n) {
+    printf("Sending packet %d to receiver\n", base);
+    sleep(timer);
 
-    if (head == NULL)
-        head = newNode;
-    else {
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = newNode;
+    if (rand() % 10 < 2) {
+      printf("Acknowledgement of packet %d has not been received\n", base);
+      sleep(1);
+
+      for (int i = base + 1; i < base + windowSize && i < n; i++) {
+        printf("Sending packet %d to receiver\n", i);
+        printf("Packet %d has been dropped by receiver\n", i);
+        sleep(1);
+      }
+    } else {
+      printf("Acknowledgement for packed %d has been received\n", base);
+      base++;
     }
+  }
 
-    newNode->next = NULL;
-}
-
-void main() {
-    srand(time(NULL));
-
-    printf("Enter Total Number Of Frames: ");
-    int n;
-    scanf("%d", &n);
-
-    printf("Enter Timer (In Sec): ");
-    int timer;
-    scanf("%d", &timer);
-
-    printf("Enter Window Size: ");
-    int size;
-    scanf("%d", &size);
-
-    int i = 0;
-    for (i = 0; i < size; i++)
-        addNode(i);
-
-    while (head != NULL) {
-        printf("Sending Packet %d To Receiver\n", head->packet);
-        sleep(timer);
-
-        int lost = rand() % n;
-        if (lost == head->packet) {
-            printf("Acknowledgement For Packet %d Has Not Been Received\n", head->packet);
-            sleep(1);
-
-            struct node* temp = head->next;
-            while (temp != NULL) {
-                printf("Sending Packet %d To Receiver\n", temp->packet);
-                sleep(1);
-                printf("Packet %d Has Been Discarded By The Receiver\n", temp->packet);
-                temp = temp->next;
-            }
-            printf("Retransmitting Packet %d Back To Receiver\n", head->packet);
-        } else {
-            printf("Acknowledgement For Packet %d Has Been Received\n", head->packet);
-
-            struct node* temp = head;
-            head = head->next;
-            if (i < n) {
-                addNode(i);
-                i++;
-            }
-            free(temp);
-        }
-    }
+  return 0;
 }
